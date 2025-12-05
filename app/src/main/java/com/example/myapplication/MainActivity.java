bb4ca3a608f6256ef0a7867f2c1b00a9e25786f2
mechaniktgmobilne <marzkup@gmail.com>
2025-12-05 12:11:25 +0100

    polaczenie z Internetem i odczyt pytan wyswietlenie tresci w toast

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,16 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,97 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://my-json-server.typicode.com/mechaniktgmobilne/retrofit_pytania/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        JsonPlaceHolderApi jsonPlaceHolderApi
+                = retrofit.create(JsonPlaceHolderApi.class);
+        Call<List<Pytanie>> call = jsonPlaceHolderApi.getPytania();
+        call.enqueue(
+                new Callback<List<Pytanie>>() {
+                    @Override
+                    public void onResponse(Call<List<Pytanie>> call, Response<List<Pytanie>> response) {
+                        if(!response.isSuccessful()){
+                            Toast.makeText(MainActivity.this, response.code(), Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        List<Pytanie> pytania = response.body();
+                        Toast.makeText(MainActivity.this,
+                                pytania.get(0).getTrescPytania(), Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<Pytanie>> call, Throwable t) {
+                        Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
     }
 }
+/*
+      call.enqueue(
+                new Callback<List<Pytanie>>() {
+                    @Override
+                    public void onResponse(Call<List<Pytanie>> call, Response<List<Pytanie>> response) {
+
+                        if(!response.isSuccessful()){
+
+
+                            Toast.makeText(MainActivity.this,
+
+
+                                    ""+response.code(),
+
+
+                                    Toast.LENGTH_SHORT).show();
+
+
+                            return;
+
+
+                        }
+
+
+                        pytania = response.body();
+
+
+                        Toast.makeText(MainActivity.this,
+
+
+                                pytania.get(0).getTrescPytania()
+
+
+                                , Toast.LENGTH_SHORT).show();
+
+
+                    }
+
+
+
+
+
+                    @Override
+
+
+                    public void onFailure(Call<List<Pytanie>> call, Throwable t) {
+
+
+                        Toast.makeText(MainActivity.this,
+
+
+                                ""+t.getMessage(), Toast.LENGTH_SHORT)
+
+
+                                .show();
+
+
+                    }
+
+
+                }
+
+
+        );
+ */
